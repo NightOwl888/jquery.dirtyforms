NightOwl888/jquery.dirtyforms

commit 26fdfec892ab479606693942a39f89f169fa611c

Fixes #49, Chrome and Safari show browser confirm dialog in addition to the GUI dialog.

jquery.dirtyforms.js
@@ -469,7 +469,7 @@ if (typeof jQuery == 'undefined') throw ("jQuery Required");
 	}
 
 	var decidingContinue = function(ev){
-		window.onbeforeunload = null; // fix for chrome
+	    clearUnload(); // fix for chrome/safari
 		ev.preventDefault();
 		settings.dialogStash = false;
 		$(document).trigger('decidingcontinued.dirtyforms');

commit c7a9ecfb324c00120d54fe15d880e21a7b70ead1

Correcting commit 26fdfec892ab479606693942a39f89f169fa611c (patch for #49), which caused the beforeunload.dirtyforms event to fire twice when continuing from the dialog.

jquery.dirtyforms.js
@@ -542,8 +542,7 @@ License MIT
     };
 
     var refire = function (ev) {
-        $(document).trigger('refire.dirtyforms', [ev])
-                   .trigger('beforeunload.dirtyforms');
+        $(document).trigger('refire.dirtyforms', [ev]);
         if (ev.type === 'click') {
             dirtylog("Refiring click event");
             events.onRefireClick(ev);